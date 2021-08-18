2d494d9a105bbc35eecd28d829004679bfbac9d8
Chris-AM <c.aranguizm@outlook.com>
2021-08-17 20:54:00 -0400

    new componentes created

---
+++ src/app/pages/graphycs1/graphycs1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphycs1',
+  templateUrl: './graphycs1.component.html',
+  styles: [
+  ]
+})
+export class Graphycs1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
