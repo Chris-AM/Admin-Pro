e7442f7707b9abbf62ce5d3c447ed2868e0ac0a6
Chris-AM <c.aranguizm@outlook.com>
2021-08-17 21:44:09 -0400

    creating children paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Graphycs1Component } from './pages/graphycs1/graphycs1.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
- {path: 'dashboard', component: DashboardComponent},
- {path: 'login', component: LoginComponent},
- {path: 'register', component: RegisterComponent},
- {path: 'progress', component: ProgressComponent},
- {path: 'graphycs1', component: Graphycs1Component},
- {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
- {path: '**', component: PageNotFoundComponent }
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphycs1', component: Graphycs1Component },
+    ],
+  },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [ RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
