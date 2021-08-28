b9e69afc163783a562c8603f4dd1cde83879d63a
Chris-AM <c.aranguizm@outlook.com>
2021-08-28 19:53:28 -0400

    Sidebar updated to a dynamic one

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SidebarService {
+  menu: any[] = [
+    {
+      title: 'Dashboard',
+      icon: 'mdi mdi-guage',
+      subMenu: [
+        { title: 'Main', url: '/' },
+        { title: 'Your Progress', url: 'progress' },
+        { title: 'Your Graphycs', url: 'graphycs1' },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
