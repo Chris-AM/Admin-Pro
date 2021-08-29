a17d577d6662246bb942014bb757b7dba5b371ac
Chris-AM <c.aranguizm@outlook.com>
2021-08-29 03:05:57 -0400

    learning rxjs with manual and automatic observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { retry,take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RxjsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+   this.returnInterval().subscribe(console.log);
+    ////////////////////////////////////////
+    ////////// Manual Observable ///////////
+    ////////////////////////////////////////
+    /* this.returnObservable()
+      .pipe(retry())
+      .subscribe(
+      (value) => console.log('ticks', value),
+      (error) => console.warn('error', error),
+      () => console.info('Interval finished')
+    ); */
   }
 
+  ngOnInit(): void {}
+
+  returnInterval(): Observable<number> {
+    return interval(1000)
+            .pipe(
+              take(4),
+              map(value => value + 1)
+            );
+  }
+
+  returnObservable(): Observable<number> {
+    let i = -1;
+    return new Observable<number>((observer) => {
+      const interval = setInterval(() => {
+        i++;
+        observer.next(i);
+        if (i === 8) {
+          clearInterval(interval);
+          observer.complete();
+        } else if (i === 2) {
+          observer.error('Observer is in position 2');
+        }
+      }, 1000);
+    });
+   
+  }
 }
